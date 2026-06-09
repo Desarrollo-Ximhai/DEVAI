0103dade93f587a23c28e2196ced808a22a15c6e
Desarrollo-Ximhai <145365776+Desarrollo-Ximhai@users.noreply.github.com>
2026-06-09 13:24:28 -0600

    regresando al commit que jalaba

--- accionesGemini.py
+++ accionesGemini.py
@@ -37,21 +37,5 @@ def generate_response(prompt, model_name="models/gemini-3.1-flash-lite", archivo
     convo = chat_model.start_chat()
     response = convo.send_message(contenidos_payload)
     payload_total_tokens = contenidos_payload + [response.text]
-    
-    # ─── EXTRACCIÓN DE TOKENS ──────────────────────────────────────
-    # Accedemos a los metadatos de uso que Gemini ya calculó para nosotros
-    uso_tokens = response.usage_metadata
-    
-    tokens_entrada = uso_tokens.prompt_token_count
-    tokens_salida = uso_tokens.candidates_token_count
-    tokens_totales = uso_tokens.total_token_count
-    
-    print(f"--- Métrica de Tokens ---")
-    print(f"---- Response completo ---")
-    print(response)
-    print(f"Tokens de Entrada (Prompt + Archivos): {tokens_entrada}")
-    print(f"Tokens de Salida (Respuesta): {tokens_salida}")
-    print(f"Tokens Totales: {tokens_totales}")
-    print(f"─────────────────────────")
-
+    tokens = chat_model.count_tokens(payload_total_tokens)
     return response.text

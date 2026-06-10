3f5544de039fb62481b032e138dcaf67b53e061e
Desarrollo-Ximhai <145365776+Desarrollo-Ximhai@users.noreply.github.com>
2026-06-10 15:37:16 -0600

    Con reranker de langsearch 2

--- main.py
+++ main.py
@@ -254,7 +254,7 @@ def query_rag(user_query: str, memoria, chat_id:int, codigo, bd, archivo, proyec
         collection_memory = memoria
         chunksCodigo = search_in_qdrant(client, codigo, user_query, query_embedding, None, k=25 )
         chunksBD = search_in_qdrant(client, bd,  user_query, query_embedding768, proyecto , k=25)
-        chunksArchivo = search_in_qdrant(client, archivo,  user_query, query_embedding768, None, k=0)
+        chunksArchivo = search_in_qdrant(client, archivo,  user_query, query_embedding768, None, k=10)
 
         memory = getProjectMemory(
             client=client,

cfda0d7c5efa1210838dd9bf7008cdc26512395b
Desarrollo-Ximhai <145365776+Desarrollo-Ximhai@users.noreply.github.com>
2026-06-10 13:10:37 -0600

    Con busqueda dispersa en qdrant 7

--- main.py
+++ main.py
@@ -249,7 +249,7 @@ def query_rag(user_query: str, memoria, chat_id:int, codigo, bd, archivo, proyec
             return {'error': 'Failed to generate embedding 768 for query'}, 500
 
         collection_memory = memoria
-        chunksCodigo = search_in_qdrant(client, codigo, user_query, query_embedding, proyecto, k=8 )
+        chunksCodigo = search_in_qdrant(client, codigo, user_query, query_embedding, None, k=8 )
         chunksBD = search_in_qdrant(client, bd,  user_query, query_embedding768, proyecto , k=10)
         chunksArchivo = search_in_qdrant(client, archivo,  user_query, query_embedding768, proyecto, k=5)
 

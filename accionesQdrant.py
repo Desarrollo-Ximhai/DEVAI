7cdae1de1ef0078a2382cce72b3ba226907cf54c
Desarrollo-Ximhai <145365776+Desarrollo-Ximhai@users.noreply.github.com>
2026-06-15 12:36:13 -0600

    Metodo para subir proyectos y chunkear(fase 2)

--- accionesQdrant.py
+++ accionesQdrant.py
@@ -278,7 +278,7 @@ def embebirBaseDatos(descripcion, archivo, proyecto):
 
     
     respuesta = generate_response(prompt)
-    diccionario_descripciones = json.loads(respuesta_api)
+    diccionario_descripciones = json.loads(respuesta)
 
     for chunk in chunks_base:
         if chunk["metadata"]["type"] == "table":

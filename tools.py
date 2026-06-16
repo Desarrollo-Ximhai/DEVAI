5e126e141c7942145642c52680c7371f7e5c72fe
Desarrollo-Ximhai <145365776+Desarrollo-Ximhai@users.noreply.github.com>
2026-06-16 12:46:21 -0600

    Con function calling Fase Pre

--- tools.py
+++ tools.py
@@ -34,7 +34,7 @@ class AgenteTools:
             return {'error': 'Failed to generate embedding 768 for query'}, 500
 
 
-        puntos_ganadores = self.ObjQdrant.search_in_qdrant(user_query=query, query_embedding=query_embedding768, k=40)
+        puntos_ganadores = self.ObjQdrant.search_in_qdrant(user_query=query, query_embedding=query_embedding768, k=20)
         
         # Mapeamos a texto limpio para la IA
         contexto_para_el_agente = []

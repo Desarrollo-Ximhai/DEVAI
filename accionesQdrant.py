d9949b86b2fd2345f1ad31cf264093a300d8c958
Desarrollo-Ximhai <145365776+Desarrollo-Ximhai@users.noreply.github.com>
2026-06-22 10:27:30 -0600

    Debuggeando Reranker

--- accionesQdrant.py
+++ accionesQdrant.py
@@ -39,14 +39,17 @@ def rerank_con_langsearch( query_usuario, candidatos, top_n=10):
             "Content-Type": "application/json"
         }
         print('documentos reranker')
-        print(candidatos)
+        #print(candidatos)
 
         # Extraemos solo las cadenas de texto limpias de los candidatos de Qdrant
         documentos = [
             c.payload.get("text", "") if hasattr(c, "payload") else c.get("text", "")
             for c in candidatos
         ]
-       
+        print(len(documentos))
+        print(type(documentos))
+        print(documentos)
+
         payload = {
             "model": "langsearch-reranker-v1",
             "query": query_usuario,

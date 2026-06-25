402520b01aa66829b844029d719f72299f9e14aa
Desarrollo-Ximhai <145365776+Desarrollo-Ximhai@users.noreply.github.com>
2026-06-25 15:01:53 -0600

    STREAMING FASE 1

--- main.py
+++ main.py
@@ -304,14 +304,14 @@ async def devai_endpoint(request: Request):
         # Guardado en la memoria de Qdrant  
         uuids = []
         # if textoRespuesta.strip():
-        uuids = await objMemoria.save_to_qdrant(
-            embed_fn=embed_with_gemini,
-            user_query=query,
-            collection_memory=memoria,
-            respuesta=textoRespuesta,
-            chat_id=chat_id,
-            proyecto=proyecto
-        )
+        # uuids = await objMemoria.save_to_qdrant(
+        #     embed_fn=embed_with_gemini,
+        #     user_query=query,
+        #     collection_memory=memoria,
+        #     respuesta=textoRespuesta,
+        #     chat_id=chat_id,
+        #     proyecto=proyecto
+        # )
 
         respuesta_final_metadata = {
             "type": "final_metadata",

96ca80baae66eb0ec2d24a91e8a6a0258c606ee2
Desarrollo-Ximhai <145365776+Desarrollo-Ximhai@users.noreply.github.com>
2026-07-02 10:21:47 -0600

    STREAMING FASE 2

--- main.py
+++ main.py
@@ -241,7 +241,8 @@ async def devai_endpoint(request: Request):
             })
     #El historial lo aplanamos al numero de tokens que traemos por defecto
     historialModificado = optimizar_y_aplanar_historial(historial, max_tokens)
-
+    debug('archivos_procesados')
+    debug(archivos_procesados)
     #Objetos de Qdrant para pasarselos a las tools. 
     objQdrant = Qdrant(
         client=client,  
@@ -307,14 +308,14 @@ async def devai_endpoint(request: Request):
         uuids = []
         debug(f"Texto Respuesta: {textoRespuesta.strip()}")
 
-        uuids = await objMemoria.save_to_qdrant(
-            embed_fn=embed_with_gemini,
-            user_query=query,
-            collection_memory=memoria,
-            respuesta=textoRespuesta.strip(),
-            chat_id=chat_id,
-            proyecto=proyecto
-        )
+        # uuids = await objMemoria.save_to_qdrant(
+        #     embed_fn=embed_with_gemini,
+        #     user_query=query,
+        #     collection_memory=memoria,
+        #     respuesta=textoRespuesta.strip(),
+        #     chat_id=chat_id,
+        #     proyecto=proyecto
+        # )
 
         respuesta_final_metadata = {
             "type": "final_metadata",

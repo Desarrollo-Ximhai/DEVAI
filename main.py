6711c699105e71e6a04689747a501c221043714f
Desarrollo-Ximhai <145365776+Desarrollo-Ximhai@users.noreply.github.com>
2026-08-20 10:01:42 -0600

    CRAWLER FASE 3

--- main.py
+++ main.py
@@ -759,12 +759,12 @@ async def endpoint_crawl(request: Request):
     ) 
     textos_limpios = []
     # for pagina in paginas_extraidas:
-    # prompt = f"{system_instruction}. \n\n TEXTO:{pagina}"
-    # texto = await generate_response_litellm_simple( prompt = prompt , model_name= modelo, proxy_key=LITELLM_PROXY_KEY, proxy_url=LITELLM_PROXY_URL )
-    # if(texto['type'] == 'error'):
-    #     pass
-    # textos_limpios.append(texto['content']) 
-    debug(paginas_extraidas)
+    prompt = f"{system_instruction}. \n\n TEXTO:{pagina}"
+    texto = await generate_response_litellm_simple( prompt = prompt , model_name= modelo, proxy_key=LITELLM_PROXY_KEY, proxy_url=LITELLM_PROXY_URL )
+    if(texto['type'] == 'error'):
+        pass
+    textos_limpios.append(texto['content']) 
+    #debug(paginas_extraidas)
     return {
         "status": "success",
         "total_paginas": len(paginas_extraidas),
